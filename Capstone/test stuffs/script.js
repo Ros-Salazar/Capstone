82fd298fe99f7676a11ae68bf7d6b9c32079c586
unknown <corpuzlex213@gmail.com>
2024-12-17 11:11:03 +0800

    Done setting up firebase(?)

--- Capstone/test stuffs/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('addProject').addEventListener('click', function() {
-    document.getElementById('projectForm').style.display = 'block';
-});
-
-document.getElementById('cancelBtn').addEventListener('click', function() {
-    document.getElementById('projectForm').style.display = 'none';
-});
